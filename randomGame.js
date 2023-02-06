1a3a303f818b73675aed8e13aae37b50f3de34fa
Your Name <you@example.com>
2023-02-05 21:13:48 -0500

    exercise 1

--- randomGame.js
+++ randomGame.js
@@ -1,29 +1,16 @@
 
 function randomGame() {
-
-    let tries=0
- 
-     setInterval(function () {
- 
-       let number= Math.random();
- 
-        
- 
+    let tries = 0
+    setInterval(function () {
+        let number = Math.random();
         if (number > 0.75) {
- 
-         console.log('Number:'+ number+' It took '+tries+' try/tries.')
- 
-          clearInterval(this)
-          }else{
-            tries=tries+1
-            console.log('try #: '+tries)
-          }
-          
-         
-         
- 
-     }, 1000)
- 
- }
- 
- randomGame()
+            console.log('Number:' + number + ' It took ' + tries + ' try/tries.')
+            clearInterval(this)
+        } else {
+            tries++
+            console.log('try #: ' + tries)
+        }
+    }, 1000)
+}
+
+randomGame()
